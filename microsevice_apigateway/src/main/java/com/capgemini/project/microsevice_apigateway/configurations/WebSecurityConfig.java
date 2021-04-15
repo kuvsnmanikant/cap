155ca284fee5281de116f368bb14e964c2f46d32
kuvsnmanikant <69764188+kuvsnmanikant@users.noreply.github.com>
2021-04-15 18:52:20 +0530

    Add files via upload

---
+++ microsevice_apigateway/src/main/java/com/capgemini/project/microsevice_apigateway/configurations/WebSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.capgemini.project.microsevice_apigateway.configurations;
+
+import com.capgemini.project.microsevice_apigateway.filters.JwtRequestFilter;
+import com.capgemini.project.microsevice_apigateway.models.CommonMan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public CommonMan gg(){
+		return new CommonMan();
+	}
+
+	@Autowired
+	private UserDetailsService myUserDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(myUserDetailsService);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable()
+				.authorizeRequests().antMatchers("/authenticate").permitAll().
+				// antMatchers("/hello").hasAnyAuthority("USER").
+				antMatchers("/adduser").hasAnyAuthority("ADMIN").
+						anyRequest().authenticated().and().
+						exceptionHandling().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+	}
+
+}
